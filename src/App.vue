22c07eef91eaf7ba718efeb52f906adc7d4db0f3
dmb537 <dmb537@york.ac.uk>
2021-02-05 12:25:44 +0000

    Use Google styleguide

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
     </div>
     <div id="svgSpace">
       <svg ref="box" class="box" width="100%" height="100%">
-        <rect width="100%" height="100%" fill="white" stroke="black" stroke-width="5"/>
+        <rect width="100%" height="100%"
+            fill="white" stroke="black" stroke-width="5"/>
       </svg>
     </div>
   </div>
@@ -23,7 +24,7 @@ export default {
       dragOffsetX: null,
       dragOffsetY: null,
       draggedObject: null,
-    }
+    };
   },
   computed: {
   },
@@ -31,60 +32,69 @@ export default {
   },
   methods: {
     createSvg(svgType, attributes) {
-      var svgElement = document.createElementNS("http://www.w3.org/2000/svg", svgType);
-      for (var attribute in attributes)
-        svgElement.setAttributeNS(null, attribute, attributes[attribute]);
+      const svgElement = document.createElementNS('http://www.w3.org/2000/svg', svgType);
+      for (const attribute in attributes) {
+        if (Object.prototype.hasOwnProperty.call(attributes, attribute)) {
+          svgElement.setAttributeNS(null, attribute, attributes[attribute]);
+        }
+      }
       return svgElement;
     },
     drawShape() {
-      //console.log("Adding");
+      // console.log("Adding");
       this.count += 1;
-      var randomColor = Math.floor(Math.random()*16777215).toString(16);
-      var randomX = Math.floor(Math.random() * 100);
-      var randomY = Math.floor(Math.random() * 100);
-      var newShape = this.createSvg('rect', {
-          id:`svgelement-${this.count}`,
-          x:`${randomX}`,
-          y:`${randomY}`,
-          width:20,
-          height:20,
-          fill:`#${randomColor}`,
-          cursor: 'move',
-          });
+      const randomColor = Math.floor(Math.random()*16777215).toString(16);
+      const randomX = Math.floor(Math.random() * 100);
+      const randomY = Math.floor(Math.random() * 100);
+      const newShape = this.createSvg('rect', {
+        id: `svgelement-${this.count}`,
+        x: `${randomX}`,
+        y: `${randomY}`,
+        width: 20,
+        height: 20,
+        fill: `#${randomColor}`,
+        cursor: 'move',
+      });
       newShape.addEventListener('mousedown', this.drag);
       newShape.addEventListener('mouseup', this.drop);
       this.$refs.box.appendChild(newShape);
     },
     removeShape() {
       if (this.count > 0) {
-        //console.log("removing");
-      document.getElementById(`svgelement-${this.count}`).remove();
-      this.count -= 1;
+        // console.log("removing");
+        document.getElementById(`svgelement-${this.count}`).remove();
+        this.count -= 1;
       } else {
-        //console.log("cannot remove with 0 elements");
+        // console.log("cannot remove with 0 elements");
       }
     },
     drag(event) {
       this.draggedObject = event.target.getAttribute('id');
-      this.dragOffsetX = event.offsetX - document.getElementById(this.draggedObject).getAttribute('x');
-      this.dragOffsetY = event.offsetY - document.getElementById(this.draggedObject).getAttribute('y');
+      this.dragOffsetX = event.offsetX -
+          document.getElementById(this.draggedObject).getAttribute('x');
+      this.dragOffsetY = event.offsetY -
+          document.getElementById(this.draggedObject).getAttribute('y');
       this.$refs.box.addEventListener('mousemove', this.move);
-      //console.log(`dragging id=${this.draggedObject} with offset (${this.dragOffsetX}, ${this.dragOffsetY})`);
+      // console.log(`dragging id=${this.draggedObject}
+      //    with offset (${this.dragOffsetX}, ${this.dragOffsetY})`);
     },
     drop() {
-      //console.log(`dropping id=${this.draggedObject}`);
+      // console.log(`dropping id=${this.draggedObject}`);
       this.draggedObject = this.dragOffsetX = this.dragOffsetY = null;
       this.$refs.box.removeEventListener('mousemove', this.move);
     },
     move({offsetX, offsetY}) {
-      var newX = offsetX - this.dragOffsetX;
-      var newY = offsetY - this.dragOffsetY;
-      document.getElementById(this.draggedObject).setAttributeNS(null, 'x', `${newX}`);
-      document.getElementById(this.draggedObject).setAttributeNS(null, 'y', `${newY}`);
-      //console.log(`moving id=${this.draggedObject} to (${offsetX}, ${offsetY})`);
+      const newX = offsetX - this.dragOffsetX;
+      const newY = offsetY - this.dragOffsetY;
+      document.getElementById(
+          this.draggedObject).setAttributeNS(null, 'x', `${newX}`);
+      document.getElementById(
+          this.draggedObject).setAttributeNS(null, 'y', `${newY}`);
+      // console.log(`moving id=${this.draggedObject}
+      //    to (${offsetX}, ${offsetY})`);
     },
   },
-}
+};
 </script>
 
 <style>
